2bcb62008dca43f6098a85cd15677e46044dbd9f
igorfcfs <igorfcfs@gmail.com>
2023-12-11 16:26:52 -0300

    Criando uma Listagem

--- byte-bank-alura/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ byte-bank-alura/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,9 +10,19 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection recuperarConexao(){
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/byte_bank_alura?user=root&password=root");
+            return createDataSource().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank_alura");
+        config.setUsername("root");
+        config.setPassword("root");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
--- byte-bank-alura/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ byte-bank-alura/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -2,12 +2,9 @@ package br.com.alura.bytebank.domain.conta;
 
 import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
-import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,13 +12,15 @@ public class ContaService {
 
     private Set<Conta> contas = new HashSet<>();
 
-    public Set<Conta> listarContasAbertas() {
-        return contas;
-    }
-
     private ConnectionFactory connection;
+
     public ContaService() {
         this.connection = new ConnectionFactory();
+    }
+
+    public Set<Conta> listarContasAbertas() {
+        Connection conn = connection.recuperarConexao();
+        return new ContaDAO(conn).listar();
     }
 
     public BigDecimal consultarSaldo(Integer numeroDaConta) {
@@ -29,36 +28,7 @@ public class ContaService {
         return conta.getSaldo();
     }
 
-//    public void abrir(DadosAberturaConta dadosDaConta) {
-//        var cliente = new Cliente(dadosDaConta.dadosCliente());
-//        var conta = new Conta(dadosDaConta.numero(), cliente);
-//        if (contas.contains(conta)) {
-//            throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
-//        }
-
-//        // contas.add(conta); //ao inves de adicionar o elemento em uma lista estatica, vamos INSERIR (insert) ao banco de dados
-//
-//        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" + "VALUES (?, ?, ?, ?, ?)";
-//
-//        Connection conn = connection.recuperarConexao();
-//
-//        //QUERY - Consulta
-//        try {
-//            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-//
-//            preparedStatement.setInt(1, conta.getNumero());
-//            preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
-//            preparedStatement.setString(3, dadosDaConta.dadosCliente().nome());
-//            preparedStatement.setString(4, dadosDaConta.dadosCliente().cpf());
-//            preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
-//
-//            preparedStatement.execute();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
-    public void abrir(DadosAberturaConta dadosDaConta){
+    public void abrir(DadosAberturaConta dadosDaConta) {
         Connection conn = connection.recuperarConexao();
         new ContaDAO(conn).salvar(dadosDaConta);
     }
@@ -95,10 +65,12 @@ public class ContaService {
     }
 
     private Conta buscarContaPorNumero(Integer numero) {
-        return contas
-                .stream()
-                .filter(c -> c.getNumero() == numero)
-                .findFirst()
-                .orElseThrow(() -> new RegraDeNegocioException("Não existe conta cadastrada com esse número!"));
+        Connection conn = connection.recuperarConexao();
+        Conta conta = new ContaDAO(conn).listarPorNumero(numero);
+        if(conta != null) {
+            return conta;
+        } else {
+            throw new RegraDeNegocioException("Não existe conta cadastrada com esse número!");
+        }
     }
 }
